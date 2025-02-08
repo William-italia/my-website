00c6f9006e55fe3ebd571129e08e7b5f3ebe7d53
William Italia <williamitalia70@outlook.com>
2025-02-08 17:32:49 -0300

    fix: arrumando bug de animação do menu-box

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@ const menuBar = document.getElementById("menu-bar");
 const menuBox = document.getElementById("menu-box");
 
 menuBar.addEventListener("click", () => {
-    if (menuBox.classList.contains("h-0")) {
-        menuBox.classList.remove("w-0", "h-0", "overflow-hidden");
-        menuBox.classList.add("w-[200px]", "h-auto");
+    if (menuBox.classList.contains("overflow-hidden")) {
+        menuBox.classList.remove("w-0", "overflow-hidden");
+        menuBox.classList.add("w-[200px]");
     } else {
-        menuBox.classList.remove("w-[200px]", "h-auto");
-        menuBox.classList.add("w-0", "h-0", "overflow-hidden");
+        menuBox.classList.remove("w-[200px]");
+        menuBox.classList.add("w-0", "overflow-hidden");
     }
 });
