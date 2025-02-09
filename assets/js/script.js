03605e0f189cfbebc42e82163f83c28c37bb0519
William Italia <williamitalia70@outlook.com>
2025-02-08 21:21:47 -0300

    dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // const menu = document.getElementById('menu-bar');
 // const menuBox = document.getElementById('menu-box');
 
-
+const body = document.body;
+const boxTheme = document.getElementById("box-theme");
 const menuBar = document.getElementById("menu-bar");
 const menuBox = document.getElementById("menu-box");
 const box = document.getElementById('box');
@@ -16,9 +17,21 @@ menuBar.addEventListener("click", () => {
     }
 });
 
+boxTheme.addEventListener('click', (e) => {
+    body.classList.toggle('dark');
+
+    if(body.classList.contains('dark')) {
+        body.classList.remove('bg-[#F0E7DB]');
+        body.classList.add('bg-[#202023]');
+    } else {
+        body.classList.remove('bg-[#202023]');
+        body.classList.add('bg-[#F0E7DB]');
+    }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     // box.classList.remove("hidden");
     box.classList.remove("opacity-0", "top-[600px]");
     // box.classList.add('show');
 });
+
